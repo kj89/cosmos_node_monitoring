d4d77f894542848a18c1c658bb2a8a2c36cac15d
valNodeOne <81831703+valNodeOne@users.noreply.github.com>
2022-07-20 16:58:47 +0200

    Update install_monitoring.sh
    There have been permission issues while using the script. Changing the permissions of docker-archive-keyring.gpg fixes this.

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -32,6 +32,7 @@ then
     echo -e "\e[1m\e[32m3.1 Installing Docker... \e[0m" && sleep 1
     sudo apt-get install ca-certificates curl gnupg lsb-release wget -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sudo chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y
