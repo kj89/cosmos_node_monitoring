db31bdd85db8848f08a9cb372e9491f3bbe3e7ac
philipsu522 <philip.su.522@gmail.com>
2022-08-10 09:37:00 -0700

    fix

--- upgrade-checker.sh
+++ upgrade-checker.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 NODE=$1
 echo "$(date +%D-%H:%M:%S)" "Checking for any upgrades"  >> /var/log/upgrade-checker.log
-echo $(~/go/bin/seid q upgrade plan --output json --node tcp://$NODE:26657| jq -r .height)
-status=$?
-if [ $status -eq 0 ]; then
+~/go/bin/seid q upgrade plan --output json --node tcp://$NODE:26657
+if [ $? -eq 0 ]; then
   HEIGHT=$(~/go/bin/seid q upgrade plan --output json --node tcp://$NODE:26657| jq -r .height)
   echo "$(date +%D-%H:%M:%S)" "Upgrade found at $HEIGHT. Setting alert and restart alertmanager"
   UPGRADE_HEIGHT=$HEIGHT envsubst '${UPGRADE_HEIGHT}' < /home/ubuntu/sei-node-monitoring/prometheus/alerts/alert.rules.TEMPLATE > /home/ubuntu/sei-node-monitoring/prometheus/alerts/alert.rules
